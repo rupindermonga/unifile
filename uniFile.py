10fa11300ea3e4aeef8e41a97c77ad26b6cf9e76
Rupinder Monga <rupinder@localhost.localdomain>
2019-11-30 13:39:03 +0530

    coding and decoding

---
+++ uniFile.py
@@ -0,0 +1,14 @@
+CODEC = 'utf-8'
+FILE = 'unicode.txt'
+
+hello_out = "Hello World\n"
+bytes_out = hello_out.encode(CODEC)
+f=open(FILE,"wb")
+f.write(bytes_out)
+f.close()
+
+f=open(FILE,"rb")
+bytes_in = f.read()
+f.close()
+hello_in = bytes_in.decode(CODEC)
+print(hello_in)
